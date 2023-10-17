c70cf7f7b3abc49e41ddc1a9341e8ff88ca6dd16
neelindresh <indresh2neel@gmail.com>
2023-10-17 19:49:19 +0530

    initial commit

---
+++ config.py
@@ -0,0 +1,33 @@
+import openai
+import os
+
+
+
+api_key='fb3a0027e2c64a52adae3ba9f4b67583'
+
+#Responce
+deployment_name="LH-GPT"
+api_type = "azure"
+api_version = "2023-06-01-preview" 
+api_base = "https://openai-lh.openai.azure.com/"  # Your Azure OpenAI resource's endpoint value.
+
+
+#Embeddings
+EMB_DEPLOYMENT_NAME="LH-embedding"
+EMB_MODEL="text-embedding-ada-002"
+
+
+#Hugginface_T5_Model
+HUGGINFACE_MODEL=None
+HUGGINFACE_MODEL_OFFLOAD=None
+HUGGINFACE_MODEL_PARAMS={}
+
+
+BASE_PATH=os.getcwd()
+MODEL_FOLDER="models"
+EMD_FOLDER="E5-base-v2"
+#EMD_PATH=os.path.join(BASE_PATH,MODEL_FOLDER,EMD_FOLDER)
+EMD_PATH="intfloat/e5-base-v2"
+DATA_FOLDER="data"
+DATA_PATH=os.path.join(BASE_PATH,DATA_FOLDER)
+
